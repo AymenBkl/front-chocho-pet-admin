a03111ea1930ddfa9a9dfc3143220b07899e62d4
Aymen Abdelmoumen Boukhelkhal <a.boukhalkhel@esi-sba.dz>
2021-05-28 22:18:33 +0200

    adding font-awesome

--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -3,28 +3,24 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from '../../pages/dashboard/dashboard.component';
 import { UserComponent } from '../../pages/user/user.component';
 import { TableComponent } from '../../pages/table/table.component';
-import { TypographyComponent } from '../../pages/typography/typography.component';
-import { IconsComponent } from '../../pages/icons/icons.component';
-import { MapsComponent } from '../../pages/maps/maps.component';
 import { NotificationsComponent } from '../../pages/notifications/notifications.component';
-import { UpgradeComponent } from '../../pages/upgrade/upgrade.component';
 import { AuthGuardService as AuthGuard } from '../../services/auth-guard.service';
-import { HashInfoComponent } from 'app/pages/hash-info/hash-info.component';
-import { ComplaintsComponent } from 'app/pages/complaints/complaints.component';
-import { WithdrawsComponent } from 'app/pages/withdraws/withdraws.component';
-import { OrdersComponent } from 'app/pages/orders/orders.component';
 import { ProductsComponent } from 'app/pages/products/products.component';
 import { ApiLogsComponent } from 'app/pages/api-logs/api-logs.component';
 import { LogsDetailComponent } from 'app/components/logs-detail/logs-detail.component';
+import { SubscribersComponent } from 'app/pages/subscribers/subscribers.component';
+import { ContactsComponent } from 'app/pages/contacts/contacts.component';
 
 
 export const AdminLayoutRoutes: Routes = [
   //{ path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
   { path: 'admin', component: UserComponent, canActivate: [AuthGuard] },
-  { path: 'orders', component: OrdersComponent, canActivate: [AuthGuard] },
-  { path: 'products', component: ProductsComponent, canActivate: [AuthGuard] },
-  { path: 'logs', component: ApiLogsComponent, canActivate: [AuthGuard] },
-  { path: 'logs/:link', component: LogsDetailComponent, canActivate: [AuthGuard] },
+  { path: 'subscribers', component: SubscribersComponent , canActivate: [AuthGuard] },
+  { path: 'contacts', component: ContactsComponent, canActivate: [AuthGuard] },
+  //{ path: 'orders', component: OrdersComponent, canActivate: [AuthGuard] },
+  //{ path: 'products', component: ProductsComponent, canActivate: [AuthGuard] },
+  //{ path: 'logs', component: ApiLogsComponent, canActivate: [AuthGuard] },
+  //{ path: 'logs/:link', component: LogsDetailComponent, canActivate: [AuthGuard] },
   //{ path: 'hashes', component: TableComponent, canActivate: [AuthGuard] },
   //{ path: 'typography', component: TypographyComponent, canActivate: [AuthGuard] },
   //{ path: 'icons', component: IconsComponent, canActivate: [AuthGuard] },
