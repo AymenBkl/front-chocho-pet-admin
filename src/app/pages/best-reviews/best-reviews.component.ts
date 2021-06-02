425bfa34ebc5296d9ede9a67f5ee5cf35b8603ef
Aymen Abdelmoumen Boukhelkhal <a.boukhalkhel@esi-sba.dz>
2021-06-02 06:18:56 +0200

    submit forn

--- src/app/pages/best-reviews/best-reviews.component.ts
+++ src/app/pages/best-reviews/best-reviews.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from "jquery";
 
 @Component({
   selector: 'app-best-reviews',
@@ -8,17 +9,37 @@ import { Component, OnInit } from '@angular/core';
 export class BestReviewsComponent implements OnInit {
 
   bestReviewsForm:{mainImgUrl:'',descriptionReview:'',authorReview:'',status:''};
+  formFields = [];
   constructor() { }
 
   ngOnInit(): void {
+    this.addFormField();
+  }
+
+  addFormField(fields = {mainImgUrl:'',descriptionReview:'',authorReview:'',status:''}) {
+    let lenghtFormField = this.formFields.length;
+    this.formFields.push({
+      value:fields,
+      formId:lenghtFormField + 1
+    })
+  }
+
+  addAnotherForm() {
+    this.addFormField();
   }
 
 
   openUploadFile(id,type) {
-
   }
 
   onChangeSelect(event,id) {
-
+    if (event == 'url') {
+      $(`#review-form-${id}`).find('.field-url-review').show();
+      $(`#review-form-${id}`).find('.field-file-review').hide();
+    }
+    else if (event == 'file'){
+      $(`#review-form-${id}`).find('.field-url-review').hide();
+      $(`#review-form-${id}`).find('.field-file-review').show();
+    }
   }
 }
