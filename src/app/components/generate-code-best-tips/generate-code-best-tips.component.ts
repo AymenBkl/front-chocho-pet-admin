8d1650e51e4b969933263ac76d763c4a09f02792
Aymen Abdelmoumen Boukhelkhal <a.boukhalkhel@esi-sba.dz>
2021-06-03 18:03:02 +0200

    slick carousel worked preview

--- src/app/components/generate-code-best-tips/generate-code-best-tips.component.ts
+++ src/app/components/generate-code-best-tips/generate-code-best-tips.component.ts
@@ -17,15 +17,16 @@ export class GenerateCodeBestTipsComponent implements OnInit {
   segmentToShow : string = 'code';
   bestTipsCode: string = '';
   slideCarouselConfig = {
-    infinite: true,
-    autoplay: false,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    pauseOnHover: true,
-    arrows: true,
-    prevArrow: '<button type="button" class="slick-prev-carousel"><img class="left_arrow" src="https://cdn.shopify.com/s/files/1/0254/2937/7112/files/Icon_feather-arrow-left-circle-1.png?v=1618817807"></button>',
-    nextArrow: '<button type="button" class="slick-next-carousel"><img class="right_arrow" src="https://cdn.shopify.com/s/files/1/0254/2937/7112/files/Icon_feather-arrow-left-circle.png?v=1618817807"></button>',
-  }
+    "infinite": true,
+    "autoplay": false,
+    "slidesToShow": 1,
+    "slidesToScroll": 1,
+    "pauseOnHover": true,
+    "arrows": true,
+    "prevArrow": '<button type="button" class="slick-prev-carousel"><img class="left_arrow" src="https://cdn.shopify.com/s/files/1/0254/2937/7112/files/Icon_feather-arrow-left-circle-1.png?v=1618817807"></button>',
+    "nextArrow": '<button type="button" class="slick-next-carousel"><img class="right_arrow" src="https://cdn.shopify.com/s/files/1/0254/2937/7112/files/Icon_feather-arrow-left-circle.png?v=1618817807"></button>',
+  };
+  bestTips : {title:'',mainImageUrl:'',description:'',status:'active',_id:'',position:number}[];
   constructor(private interactionService: InteractionService,
                       private toolsService: ToolsService,
                       private clipboard: Clipboard,
@@ -41,7 +42,8 @@ export class GenerateCodeBestTipsComponent implements OnInit {
       .then((bestTips:any) => {
         this.interactionService.closeToast();
           if (bestTips && bestTips != false){
-            this.bestTipsCode = this.buildBestTips(bestTips)
+            this.bestTips = bestTips;
+            this.bestTipsCode = this.buildBestTips()
             this.interactionService.displayToaster('Best tips Loadded Succesfully','success','LOADED');
           }
           else {
@@ -61,28 +63,28 @@ export class GenerateCodeBestTipsComponent implements OnInit {
 
 
 
-  buildBestTips(bestTips: [{title:'',mainImageUrl:'',description:'',status:'active',_id:'',position:number}]) {
-    let bestTipsCode = '<div class="best-reviews-container">';
-    let sortedBestTips = bestTips.sort((a,b) => a.position - b.position)
-    sortedBestTips.map((bestReview) => {
-      if (bestReview.status == 'active'){
+  buildBestTips() {
+    let bestTipsCode = '<div class="best-tips-container">';
+    this.bestTips = this.bestTips.sort((a,b) => a.position - b.position)
+    this.bestTips = this.bestTips.filter(bestTip => bestTip.status == 'active');
+    this.bestTips.map((bestTip) => {
         bestTipsCode += `<div class="best-tips-item-container">
+
         <div class="best-tips-image-container">
-            <img src="${bestReview.mainImageUrl}">
+            <img src="${bestTip.mainImageUrl}">
         </div>
         <div class="best-tips-text-container">
             <h1 class="header-tips-container">TIPS</h1>
 
             <div class="header-text-tips">
-                <h3>${bestReview.title}</h3>
+                <h3>${bestTip.title}</h3>
             </div>
             <div class="rating-section">
-                ${bestReview.description}
+                ${bestTip.description}
             </div>
 
         </div>
     </div>`;
-      }
     })
     bestTipsCode += '</div>';
     return bestTipsCode;
@@ -102,9 +104,7 @@ export class GenerateCodeBestTipsComponent implements OnInit {
 
   }
 
-  slickInit(e) {
-    console.log('slick initialized');
-  }
+
 
 
 
