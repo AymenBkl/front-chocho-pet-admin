956c2e92807028f723ba76cf1e90d6f72b3d75ac
Aymen Abdelmoumen Boukhelkhal <a.boukhalkhel@esi-sba.dz>
2021-06-10 15:58:10 +0200

    worked

--- src/app/components/recomneded-product/recomneded-product.component.ts
+++ src/app/components/recomneded-product/recomneded-product.component.ts
@@ -10,15 +10,39 @@ import { Product } from 'app/interface/product';
 export class RecomnededProductComponent implements OnInit {
 
   products: Product[] = [];
+  selectForms : any[] = [];
   constructor(@Inject(MAT_DIALOG_DATA) public data: {products:Product[],product:Product},) { }
 
   ngOnInit(): void {
     this.products = this.data.products;
+    this.addForm();
   }
 
 
   submitProducts(){
 
   }
+
+
+  addForm(){
+    let selectFormLength = this.selectForms.length;
+    this.selectForms.push({
+      selectOption:'None',
+      formId:selectFormLength,
+      status:'active'
+    })
+  }
+
+  deleteRecomendedProduct(id){
+    console.log(id);
+    this.selectForms[id].status = 'deleted';
+    console.log(this.selectForms[id])
+  }
+
+  activeRecomendedProduct(id){
+    this.selectForms[id].status = 'active';
+  }
+
+
 
 }
